5d518bf8712c20f199d2227f0682747f28b99557
danielk <danielk5432@gmail.com>
2025-07-25 22:23:03 +0900

    test

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ app = FastAPI()
 # 서버가 살아있는지 확인하는 기본 API (GET /)
 @app.get("/")
 def read_root():
-    return {"Hello": "World"}
+    return {"Hello": "Rouge!"}
 
 # 로컬에서 개발할 때 터미널에서 `python main.py`로 실행하기 위한 부분
 if __name__ == "__main__":
